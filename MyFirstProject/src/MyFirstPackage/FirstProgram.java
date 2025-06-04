ffe5ae7b49777dcc3b1157509bd3da5ba6fbba60
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2025-06-04 22:42:53 +0530

    Initial commit of Java project

---
+++ MyFirstProject/src/MyFirstPackage/FirstProgram.java
@@ -0,0 +1,12 @@
+package MyFirstPackage;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
