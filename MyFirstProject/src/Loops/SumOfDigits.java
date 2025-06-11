d6a8de2e97f3c40cc449e65b6f7d8e2f53fdd638
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2025-06-11 19:41:13 +0530

    Update homework.

--- MyFirstProject/src/Loops/SumOfDigits.java
+++ MyFirstProject/src/Loops/SumOfDigits.java
@@ -5,7 +5,7 @@ public class SumOfDigits {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a = 1234;
+		int a = 12345;
 		int sum = 0;
 		
 		while(a!=0) {
