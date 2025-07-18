a9c20f976c6f1f643d79ce34c13e3fb5f8570f78
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2025-07-18 11:50:14 +0530

    Selenium_Installation project.

---
+++ Selenium_Installation/src/FirstPackage/FirstProgram_Selenium.java
@@ -0,0 +1,12 @@
+package FirstPackage;
+
+import org.openqa.selenium.WebDriver;
+
+public class FirstProgram_Selenium {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
