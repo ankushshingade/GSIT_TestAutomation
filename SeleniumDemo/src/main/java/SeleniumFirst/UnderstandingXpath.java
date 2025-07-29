e4bb746d25f8aec87649b64659e48375e89a1322
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2025-07-29 21:31:12 +0530

    Updating xpath practicing.

--- SeleniumDemo/src/main/java/SeleniumFirst/UnderstandingXpath.java
+++ SeleniumDemo/src/main/java/SeleniumFirst/UnderstandingXpath.java
@@ -1,7 +1,9 @@
 package SeleniumFirst;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class UnderstandingXpath {
@@ -17,7 +19,29 @@ public class UnderstandingXpath {
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("ankush@test.com");
 		Thread.sleep(500);
-		driver.findElement(By.xpath("//label[text()='Male']")).click();
+		WebElement element = driver.findElement(By.xpath("//label[text()='Male']"));		// .click(); // gender-radio-1
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].click();", element);
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys("8765432109");
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[starts-with(@id,'date')]")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//select[@class='react-datepicker__year-select']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//option[@value='1987']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//select[@class='react-datepicker__month-select']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//option[@value='0']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//div[text()='11']")).click();
+		Thread.sleep(500);
+		String subject = "History, Data Structure, English";
+		WebElement subjects = driver.findElement(By.xpath("//div[@class='subjects-auto-complete__placeholder css-1wa3eu0-placeholder']"));
+		executor.executeAsyncScript("arguments[1].sendKeys(subject);", subjects);
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//label[text()='Reading']")).click();
 		Thread.sleep(500);
 		Thread.sleep(2000);
 		driver.quit();
